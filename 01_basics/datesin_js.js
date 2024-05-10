a1bf85c2e4e29e16153506d581a84a08d6f29497
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2024-05-10 14:04:48 +0000

    Dates and TimeStamps Practice in JavaScript

---
+++ 01_basics/datesin_js.js
@@ -0,0 +1,27 @@
+// // // console.log(Date());
+// // // let myDate = new Date();
+// // // console.log(myDate);
+// // // console.log(myDate.toDateString());
+// // // console.log(myDate.toString());
+// // // console.log(myDate.toLocaleString());
+// // // console.log(myDate.toLocaleDateString());
+// // // console.log(typeof myDate);
+
+// // // let myDate = new Date(2023, 0,23, 5 , 3)
+// // // console.log(myDate.toLocaleString());
+
+// // let myNewDate = new Date("05-09-2023")
+// // // console.log(myNewDate.toLocaleString());
+// // // //5/9/2023, 12:00:00 AM
+
+// // let myTimeStamp = Math.floor((Date.now()/1000));
+// // console.log(myTimeStamp);
+// // console.log(myNewDate.getTime());
+
+//  let newDate = new Date();
+// // console.log(newDate);
+// // console.log(newDate.getMonth()+1);
+// // console.log(newDate.getDay()+1);
+
+// newDate.toLocaleString('default',{weekday: "long",
+// })
